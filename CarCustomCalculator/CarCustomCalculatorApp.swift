374d02dcd7b6df5d466212d98aaed71ab2bd01d1
Nikita Arshinov <narshinov01@gmail.com>
2024-04-17 21:11:16 +0300

    Initial commit

---
+++ CarCustomCalculator/CarCustomCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarCustomCalculatorApp.swift
+//  CarCustomCalculator
+//
+//  Created by Nikita Arshinov on 17.04.24.
+//
+
+import SwiftUI
+
+@main
+struct CarCustomCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
